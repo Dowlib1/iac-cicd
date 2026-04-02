88e1a23abdb0aa5f725848f711b95d7cfbea8ed2
Dowlib1 <oladimeji0107@gmail.com>
2026-04-02 14:17:45 +0100

    deploy ec2

--- main.tf
+++ main.tf
@@ -83,17 +83,17 @@ resource "aws_security_group" "my_sg" {
   }
 }
 
-# 7. EC2 Instance in the Subnet
-# resource "aws_instance" "my_ec2" {
-#   ami                         = var.ami_id
-#   instance_type               = var.instance_type
-#   subnet_id                   = aws_subnet.my_subnet.id
-#   vpc_security_group_ids      = [aws_security_group.my_sg.id]
-#   associate_public_ip_address = true
+7. EC2 Instance in the Subnet
+resource "aws_instance" "my_ec2" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.my_subnet.id
+  vpc_security_group_ids      = [aws_security_group.my_sg.id]
+  associate_public_ip_address = true
 
-#   tags = {
-#     Name = "TomiwaEC2"
-#   }
-# }
+  tags = {
+    Name = "OladimejiEC2"
+  }
+}
 
 
