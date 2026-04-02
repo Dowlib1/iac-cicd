97430133ef6bc1d900b7f921c8c0a7004211ac70
Dowlib1 <oladimeji0107@gmail.com>
2026-04-02 14:34:23 +0100

    deploy ec2

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,9 @@ output "subnet_id" {
 }
 
 output "ec2_public_ip" {
-  value = aws_instance.my_ec2.public_ip
+  value = aws_instance.web_server.public_ip
 }
 
 output "ec2_id" {
-  value = aws_instance.my_ec2.id
+  value = aws_instance.web_server.id
 }
