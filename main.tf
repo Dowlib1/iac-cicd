ee51560288676415999e0529d064e586a16fca17
Dowlib1 <oladimeji0107@gmail.com>
2026-04-02 14:27:09 +0100

    deploy ec2

--- main.tf
+++ main.tf
@@ -82,8 +82,7 @@ resource "aws_security_group" "my_sg" {
     Name = "MySecurityGroup"
   }
 }
-
-# 7.  EC2 Instance in the Subnet
+# 7. EC2 Instance in the Subnet
 resource "aws_instance" "my_ec2" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
